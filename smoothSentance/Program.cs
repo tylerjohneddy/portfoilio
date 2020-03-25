44ba0c70903c0ae8ba8f0301cadd4f85a9041e58
tylerjohneddy <60263937+tylerjohneddy@users.noreply.github.com>
2020-03-25 15:59:18 +0000

    Add files via upload

---
+++ smoothSentance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the sentance you would like to be checked:");
+            String input = Console.ReadLine();
+            String output = "False";
+            string[] arrayInput = input.Split(" ");
+           
+            for(int i = 0; i<arrayInput.Length-1;i++){
+                int len = arrayInput[i].Length;
+                try{
+                    
+                   if( arrayInput[i][len-1].Equals(arrayInput[i+1][0])){
+                        output = "True";
+
+                    }
+                }catch{
+                    
+                }
+                      
+            }
+            Console.WriteLine(output);
+
+        }
+    }
+}
